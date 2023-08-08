1eaaadddf4bbac85b809cf3bdefaed454c362fa2
noaprost <kangjiwon1850@gmail.com>
2023-08-09 01:42:41 +0900

    1927 clear

--- week6/b1927.py
+++ week6/b1927.py
@@ -1,1 +1,19 @@
 # 최소 힙
+# heapq, PriorityQueue 둘다 시간초과
+# input -> sys.stdin.readline 으로 변경하니 성공
+from queue import PriorityQueue
+import sys
+
+que = PriorityQueue()
+
+n = int(input())
+
+for i in range(n):
+    item = int(sys.stdin.readline())
+    if item == 0:
+        if que.empty():
+            print(0)
+        else:
+            print(que.get())
+    else:
+        que.put(item)

915da7c75bdd789e8e6f34a3404f1d35b4b92dbd
noaprost <kangjiwon1850@gmail.com>
2023-08-12 13:30:37 +0900

    11729 clear

--- week7/b11729.py
+++ week7/b11729.py
@@ -1,3 +1,19 @@
 # 하노이 탑 이동 순서
-# 하노이의 탑 알고리즘을 분석하는게 포인트
-# 찾아보기 전에 최대한 직접 그려서 이해해보기
+
+
+# start에서 end를 거쳐 n개의 원반을 이동하는 함수
+def hanoi(n, start, end, via):
+    if n == 1:
+        move.append([start, end])
+    else:
+        hanoi(n - 1, start, via, end)
+        move.append([start, end])
+        hanoi(n - 1, via, end, start)
+
+
+n = int(input())
+move = []
+hanoi(n, 1, 3, 2)
+print(len(move))
+for m in move:
+    print(m[0], m[1])

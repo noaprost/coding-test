46bc7785bdfa778d4d8e4108c41a3ab5fe39ce45
noaprost <kangjiwon1850@gmail.com>
2023-08-19 18:03:58 +0900

    2293 clear

--- week8/b2293.py
+++ week8/b2293.py
@@ -1,1 +1,36 @@
 # 동전 1
+# 오케 3일만에 규칙을 알아냈다. 나이스
+
+import sys
+
+nn, kk = map(int, sys.stdin.readline().split())
+
+num = [int(sys.stdin.readline()) for _ in range(nn)]
+
+total = [0] * kk
+i = 0
+
+for n in range(nn):
+    for k in range(kk):
+        if n == 0:  # 초기화
+            # 사용할 수 있는 동전의 개수가 1개 이므로
+            if (k + 1) % num[n] == 0:  # 나누어떨어지면 경우의 수 1 추가
+                total[k] = 1
+            else:
+                total[k] = 0
+        else:  # 총합 값 업데이트
+            # 동전 가치보다 작은 수는 만들 수 없으므로
+            if (k + 1) < num[n]:
+                total[k] = total[k] + 0  # 경우의 수 0을 더해줌
+
+            # 동전 가치와 같은 수는 동전 1개로 표현할 수 있으므로
+            elif (k + 1) == num[n]:
+                total[k] = total[k] + 1  # 경우의 수 0을 더해줌
+
+            # 동전 가치보다 큰 수의 경우, 이전 동전들로 만들 수 있는 경우의 수가 반복되므로
+            elif (k + 1) > num[n]:
+                total[k] = total[k] + total[i]  # 이전 경우의 수를 더해줌
+                i = i + 1
+    i = 0
+
+print(total[-1])

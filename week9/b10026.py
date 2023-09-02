7ee11c370d2f72102368804c3178a0122cb8cb36
noaprost <kangjiwon1850@gmail.com>
2023-09-02 22:41:57 +0900

    10026 base code

--- week9/b10026.py
+++ week9/b10026.py
@@ -1,1 +1,19 @@
 # 적록색약
+# BFS, 큐 이용
+# 위아래양옆 검사해서 알파벳 같은거면 큐에 위치 넣고 넣은 곳은 방문 표시 후
+# 반복하다가 더이상 갈곳이 없으면 구역에 +1하고
+# 아직 방문하지 않은 다음 정점부터 다시 시작
+# 모든 정점을 방문할 때까지
+
+import sys
+from queue import Queue
+
+
+que = Queue()
+
+n = int(sys.stdin.readline())
+
+# 적록색약 map 입력
+rgbMap = [list(sys.stdin.readline().strip()) for _ in range(n)]
+
+print(rgbMap)
--- week9/b1389.py
+++ week9/b1389.py
@@ -1,9 +1,8 @@
 # 케빈 베이컨의 6단계 법칙
-# BFS 먼저
-# 안되면 플로이드 워셜 알고리즘 이용
+# BFS만으로는 안됨
+# 다익스트라 여러번 or 플로이드 워셜 알고리즘 한번 이용
 
 import sys
-from queue import PriorityQueue
 
 # 0 []
 # 1 [3, 4]
@@ -14,10 +13,7 @@ from queue import PriorityQueue
 
 node, edge = map(int, sys.stdin.readline().split())
 
-graph = [[] for _ in range(node + 1)]
-dis = [0 for _ in range(node + 1)]
-
-que = PriorityQueue()
+graph = [[] for _ in range(node)]
 
 # 노드 사이에 간선 추가
 i = 0

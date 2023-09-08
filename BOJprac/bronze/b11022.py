b02fc3b34e82c8478cf279557da7a79fc40b9750
noaprost <kangjiwon1850@gmail.com>
2023-09-08 18:15:42 +0900

    11022 clear

--- BOJprac/bronze/b11022.py
+++ BOJprac/bronze/b11022.py
@@ -4,6 +4,4 @@ n = int(sys.stdin.readline())
 
 for i in range(n):
     a, b = map(int, sys.stdin.readline().split())
-    print("Case #{0}: {1}".format(i + 1, a + b))
-
-# s = '구구단 {0} x {1} = {2}'.format(a, b, a * b)
+    print("Case #{0}: {1} + {2} = {3}".format(i + 1, a, b, a + b))

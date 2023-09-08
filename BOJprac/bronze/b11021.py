3f784042541a50199413c08d84f1cc17eadf1ffd
noaprost <kangjiwon1850@gmail.com>
2023-09-08 18:01:51 +0900

    11021 clear

---
+++ BOJprac/bronze/b11021.py
@@ -0,0 +1,9 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    print("Case #", end="")
+    print(i + 1, end="")
+    print(":", a + b)

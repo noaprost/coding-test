79078ebfd06b70e7af859536d59c3a501601e054
noaprost <kangjiwon1850@gmail.com>
2023-09-08 17:45:57 +0900

    25304 clear

---
+++ BOJprac/bronze/b25304.py
@@ -0,0 +1,15 @@
+import sys
+
+x = int(sys.stdin.readline())
+
+n = int(sys.stdin.readline())
+sum = 0
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    sum += a * b
+
+if x == sum:
+    print("Yes")
+else:
+    print("No")

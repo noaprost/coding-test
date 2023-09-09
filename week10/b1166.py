f11fcc90e0f87c1a68fc99484cd031dd76781f09
noaprost <kangjiwon1850@gmail.com>
2023-09-09 21:44:47 +0900

    1166 clear

--- week10/b1166.py
+++ week10/b1166.py
@@ -12,7 +12,7 @@ m = min(l, w, h)  # 길이의 최소값, 초기화
 
 q = [l // m, w // m, h // m]  # 몫들을 저장할 배열
 num = q[0] * q[1] * q[2]  # 담을 수 있는 개수
-
+m1 = min(l, w, h)
 while num < n:
     l1 = l / (q[0] + 1)
     l2 = w / (q[1] + 1)
@@ -20,26 +20,15 @@ while num < n:
 
     if max(l1, l2, l3) == l1:
         q[0] += 1
+        m1 = l1
     elif max(l1, l2, l3) == l2:
         q[1] += 1
+        m1 = l2
     elif max(l1, l2, l3) == l3:
         q[2] += 1
+        m1 = l3
 
     # 개수 업데이트
     num = q[0] * q[1] * q[2]
 
-a1 = l / q[0]
-a2 = w / q[1]
-a3 = h / q[2]
-
-# 가장 작은 길이보다는 작아야함
-# 작은 것들 중에서는 제일 커야함 -> 몫이
-
-t = [a1, a2, a3]
-aa = []
-qq = []
-for i in range(3):
-    if t[i] <= m:
-        aa.append(t[i])
-        qq.append(q[i])
-print(round(aa[qq.index(max(qq))], 15))
+print(m1)

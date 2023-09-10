aa06b5b2df95ddd74b44f6c4f426fb9067c34232
noaprost <kangjiwon1850@gmail.com>
2023-09-10 17:00:30 +0900

    2566 clear

---
+++ BOJprac/bronze/b2566.py
@@ -0,0 +1,21 @@
+import sys
+
+# 최대값 배열에서 가장큰 애의 위치가 행
+# 그 줄에서 어디에 index가 있는지 확인하면 열
+ai, aj = 0, 0
+a = []
+m = []
+for i in range(9):
+    tmp = list(map(int, sys.stdin.readline().split()))
+    a.append(tmp)
+    m.append(max(tmp))
+
+ai = m.index(max(m))
+
+for j in range(9):
+    if a[ai][j] == max(m):
+        aj = j
+        break
+
+print(max(m))
+print(ai, aj)

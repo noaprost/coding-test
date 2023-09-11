abd270c81e604bd4b06ac4f32264a9ea1994c480
noaprost <kangjiwon1850@gmail.com>
2023-09-11 21:09:36 +0900

    2292 clear

---
+++ BOJprac/bronze/b2292.py
@@ -0,0 +1,31 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+# 방의 개수는 1 -> 6을 제외 6개씩 늘어남
+# 1
+# 6(2~7)
+# 12(8~19)
+# 18(20~37)
+# 24(38~61)
+
+# 찾아야하는것 어느 범위에 있는지 -> 안쪽으로보터 몇번째 방에 있는지
+
+isFind = False
+curNum = 1
+roomNum = 1
+i = 1
+
+while True:
+    if n == 1:
+        break
+    else:
+        if n - curNum <= 0:
+            break
+        else:
+            roomNum += 1
+            curNum += 6 * i
+    i += 1
+
+
+print(roomNum)

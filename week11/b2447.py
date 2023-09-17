1bfc514f92057732098ca31115c1a6bca414b450
noaprost <kangjiwon1850@gmail.com>
2023-09-17 16:07:31 +0900

    2447 clear

--- week11/b2447.py
+++ week11/b2447.py
@@ -1,18 +1,32 @@
 import sys
 
 n = int(sys.stdin.readline())
-star = [["." for _ in range(n)] for _ in range(n)]
+star = [[" " for _ in range(n)] for _ in range(n)]
 
 
-def divAndCon(s, r):
-    if (int(s / 3)) > 0:
-        divAndCon(int(s / 3), r - 3)
-    for i in range(r - 3, r):
-        for j in range(r - 3, r):
-            star[i][j] = "*"
-            if i % 3 == 1 and j % 3 == 1:
-                star[i][j] = " "
+def divAndCon(x, y, l):
+    if l // 3 == 1:
+        for i in range(3):
+            for j in range(3):
+                if i == 1 and j == 1:
+                    continue
+                star[x + i][y + j] = "*"
+
+    else:
+        l2 = int(l / 3)
+        divAndCon(x, y, l2)
+        divAndCon(x + l2, y, l2)
+        divAndCon(x + l2 * 2, y, l2)
+        divAndCon(x, y + l2, l2)
+        divAndCon(x + l2 * 2, y + l2, l2)
+        divAndCon(x, y + l2 * 2, l2)
+        divAndCon(x + l2, y + l2 * 2, l2)
+        divAndCon(x + l2 * 2, y + l2 * 2, l2)
 
 
-divAndCon(n, n)
-print(star)
+divAndCon(0, 0, n)
+
+for s in star:
+    for ss in s:
+        print(ss, end="")
+    print()

cfd82c95d38c5fb244a8ca196a4d1c44b98e1655
noaprost <kangjiwon1850@gmail.com>
2023-09-26 19:56:09 +0900

    2839 clear

--- BOJprac/silver/b2839.py
+++ BOJprac/silver/b2839.py
@@ -4,3 +4,16 @@
 import sys
 
 n = int(sys.stdin.readline())
+ans = []
+five = n // 5
+three = n // 3
+
+for i in range(five + 1):
+    for j in range(three + 1):
+        if n - i * 5 - j * 3 == 0:
+            ans.append(i + j)
+
+if len(ans) == 0:
+    print(-1)
+else:
+    print(min(ans))

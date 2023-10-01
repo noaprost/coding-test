e6e211966fe9ed3f732661f739b1d95f0789af39
noaprost <kangjiwon1850@gmail.com>
2023-10-02 00:58:45 +0900

    1934 clear

---
+++ BOJprac/bronze/b1934.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    a1 = a
+    b1 = b
+    while True:
+        r = a1 % b1
+        if r == 0:
+            break
+        else:
+            a1 = b1
+            b1 = r
+
+    print(int(a * b / b1))

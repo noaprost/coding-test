a33e0eb02d87f214feedcba2c02d47d89186aa90
noaprost <kangjiwon1850@gmail.com>
2023-10-01 17:59:48 +0900

    10814 clear

---
+++ BOJprac/silver/b10814.py
@@ -0,0 +1,15 @@
+import sys
+from queue import PriorityQueue
+
+n = int(sys.stdin.readline())
+
+info = []
+
+for _ in range(n):
+    age, name = sys.stdin.readline().split()
+    info.append([age, name])
+
+info.sort(key=lambda x: int(x[0]))
+
+for i in info:
+    print(int(i[0]), i[1])

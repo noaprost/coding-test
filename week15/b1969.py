7cddd4377e3da382464dc126280fe419d85902d3
noaprost <kangjiwon1850@gmail.com>
2023-10-18 03:16:38 +0900

    10718 clear

--- week15/b1969.py
+++ week15/b1969.py
@@ -1,1 +1,14 @@
 # DNA
+# 해밍디스턴스의 합을 가장 작게 만드는 DNA를 찾는것이 목표
+import sys
+
+n, len = map(int, sys.stdin.readline().split())
+
+dna = []
+
+diff = [0] * n
+
+for _ in range(n):
+    d = sys.stdin.readline().strip()
+
+    dna.append(d)

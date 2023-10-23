b246d4ad768ffc5fb91991fb4176179b15d41e4a
noaprost <kangjiwon1850@gmail.com>
2023-10-23 17:14:13 +0900

    11727 clear

--- week08/b11726.py
+++ week08/b11726.py
@@ -11,7 +11,7 @@ n = int(sys.stdin.readline())
 p = [0 for _ in range(n + 1)]
 
 p[0] = 1
-p[1] = 1
+p[1] = 1                                                                                                                                                                    
 
 for i in range(2, n + 1):
     p[i] = p[i - 1] + p[i - 2]

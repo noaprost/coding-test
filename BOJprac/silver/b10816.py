f40d1f3b8be1b1304f12abdd18394c0d7744e27b
noaprost <kangjiwon1850@gmail.com>
2023-10-24 17:33:59 +0900

    10816 clear

--- BOJprac/silver/b10816.py
+++ BOJprac/silver/b10816.py
@@ -1,18 +1,22 @@
 import sys
-
-d = {}
+from bisect import bisect_left, bisect_right
 
 n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().split()))
-for aa in a:
-    d[int(aa)] = a.count(aa)
 
+num = list(map(int, sys.stdin.readline().split()))
+num.sort()
 
 m = int(sys.stdin.readline())
-b = list(map(int, sys.stdin.readline().split()))
 
-for bb in b:
-    if bb in d:
-        print(d[bb], end=" ")
-    else:
-        print(0, end=" ")
+findNum = list(map(int, sys.stdin.readline().split()))
+
+
+# '정렬된 리스트'에서 `값이 특정 범위에 속하는 원소의 개수`를 구할 때 좋다.
+def count_by_range(b, left_value, right_value):
+    right_index = bisect_right(b, right_value)
+    left_index = bisect_left(b, left_value)
+    return right_index - left_index
+
+
+for f in findNum:
+    print(count_by_range(num, f, f), end=" ")

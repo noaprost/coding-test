85faa0dfad690e6e6a85cc67dab09eb77ce721fa
noaprost <kangjiwon1850@gmail.com>
2023-10-25 12:34:55 +0900

    16953 clear

--- BOJprac/gold/b3190.py
+++ BOJprac/gold/b3190.py
@@ -3,31 +3,111 @@ import sys
 from collections import deque
 
 n = int(sys.stdin.readline())
-map = [[0] * n] * n
+maps = [[0 for _ in range(n)] for _ in range(n)]
 
 directions = ["T", "R", "B", "L"]
 snakeDirec = directions[1]
 
 appleLoc = int(sys.stdin.readline())
 
-x, y = map(int, sys.stdin.readline().split())
-print(x, y)
 # 사과의 위치 표시
-# for _ in range(appleLoc):
-#     x, y = map(int, sys.stdin.readline().split())
-    #map[x - 1][y - 1] = 4
+for _ in range(appleLoc):
+    x, y = map(int, sys.stdin.readline().split())
+    maps[x - 1][y - 1] = 4
 
-# l = int(sys.stdin.readline())
+l = int(sys.stdin.readline())
 
-# d = deque()
-# d.append([0, 0])
-# map[0][0] = 1
-# i = 0
-# for _ in range(l):
-#     val, rotate = sys.stdin.readline().split()
-#     while i < val:
-#         if snakeDirec == "T":
-            # [d[0][0] + 1, d[0][1]]이 경계를 넘어갔는지 -> 넘으면 종료
-            # 안넘었으면 d.appendleft([d[0][0] + 1, d[0][1]])
-            # [d[0][0] + 1, d[0][1]]에 사과가 있는지 검사 필요
-            # -> 꼬리를 없앨지 말지 검사 필요
+d = deque()
+d.append([0, 0])
+maps[0][0] = 1
+i = 1
+
+command = []
+
+for _ in range(l):
+    val, rotate = sys.stdin.readline().split()
+    command.append([val, rotate])
+
+
+for c in command:
+    val, rotate = c[0], c[1]
+    while i < int(val):
+        x = d[0][0]
+        y = d[0][1]
+        if snakeDirec == "R":
+            if y > n - 1:  # 경계를 벗어날 경우
+                exit(print(i))
+            elif maps[x][y + 1] == 1:  # 자기 몸에 닿은 경우
+                exit(print(i))
+            else:  # 경계에 닿지 않은 경우
+                if maps[x][y + 1] == 4:
+                    d.appendleft([x, y + 1])
+                    maps[x][y + 1] = 1
+                else:
+                    d.appendleft([x, y + 1])
+                    maps[x][y + 1] = 1
+                    v = d.pop()
+                    maps[v[0]][v[1]] = 0
+                i += 1
+
+        elif snakeDirec == "B":
+            if x + 1 > n - 1:  # 경계를 벗어날 경우
+                exit(print(i))
+            elif maps[x + 1][y] == 1:  # 자기 몸에 닿은 경우
+                exit(print(i))
+            else:  # 경계에 닿지 않은 경우
+                if maps[x + 1][y] == 4:
+                    d.appendleft([x + 1, y])
+                    maps[x + 1][y] = 1
+                else:
+                    d.appendleft([x + 1, y])
+                    maps[x + 1][y] = 1
+                    v = d.pop()
+                    maps[v[0]][v[1]] = 0
+                i += 1
+
+        elif snakeDirec == "L":
+            if y < 0:  # 경계를 벗어날 경우
+                exit(print(i))
+            elif maps[x][y - 1] == 1:  # 자기 몸에 닿은 경우
+                exit(print(i))
+            else:  # 경계에 닿지 않은 경우
+                if maps[x][y - 1] == 4:
+                    d.appendleft([x, y - 1])
+                    maps[x][y - 1] = 1
+                else:
+                    d.appendleft([x, y - 1])
+                    maps[x][y - 1] = 1
+                    v = d.pop()
+                    maps[v[0]][v[1]] = 0
+                i += 1
+
+        elif snakeDirec == "T":
+            if x < 0:  # 경계를 벗어날 경우
+                print("경계 아웃")
+                exit(print(i))
+            elif maps[x - 1][y] == 1:  # 자기 몸에 닿은 경우
+                print("몸에 닿음")
+                exit(print(i))
+            else:  # 경계에 닿지 않은 경우
+                if maps[x - 1][y] == 4:
+                    d.appendleft([x - 1, y])
+                    maps[x - 1][y] = 1
+                else:
+                    d.appendleft([x - 1, y])
+                    maps[x - 1][y] = 1
+                    v = d.pop()
+                    maps[v[0]][v[1]] = 0
+                i += 1
+    if rotate == "D":
+        snakeDirec = directions[(directions.index(snakeDirec) + 1) % 4]
+    elif rotate == "L":
+        didx = directions.index(snakeDirec)
+        if didx == 0:
+            snakeDirec = directions[3]
+        else:
+            snakeDirec = directions[didx - 1]
+
+for m in maps:
+    print(m)
+print(i)

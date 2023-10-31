55101f6fb466b430d7c01af41cd256b436075706
noaprost <kangjiwon1850@gmail.com>
2023-10-31 14:39:22 +0900

    9657 clear

--- week16/b5430.py
+++ week16/b5430.py
@@ -2,22 +2,25 @@
 # 필요한 함수 R, D
 import sys
 
-t = int(sys.stdin.readline())
+t = int(sys.stdin.readline()) # 테스트 케이스 개수
 
 for _ in range(t):
     reverse = False
     isError = False
-    p = sys.stdin.readline().rstrip()
-    n = int(sys.stdin.readline())
-    arr = sys.stdin.readline().rstrip()
-    if not n:
+    p = sys.stdin.readline().rstrip() # 수행할 함수 ex)RDD
+    n = int(sys.stdin.readline()) # 배열 원소 개수
+    arr = sys.stdin.readline().rstrip() # 배열
+
+    if not n: # 원소 개수가 0이면
         num = []
     else:
         num = list(map(int, list(arr[1:-1].split(","))))
+    
+    # reverse랑 delete를 도와줄 인덱스
     start = 0
     end = n - 1
 
-    for pp in p:
+    for pp in p: # 함수에서 하나씩 꺼냄
         if pp == "R":
             reverse = not reverse
         elif pp == "D":
--- week16/b9657.py
+++ week16/b9657.py
@@ -4,11 +4,11 @@ import sys
 
 n = int(sys.stdin.readline())
 
-dp = ["", "SK", "CY", "SK", "CY"]
+dp = ["", "SK", "CY", "SK", "SK"]
 
 for i in range(5, n + 1):
     if dp[i - 1] == "CY" or dp[i - 3] == "CY" or dp[i - 4] == "CY":
         dp.append("SK")
     else:
         dp.append("CY")
-print(dp[n])
+print(dp)

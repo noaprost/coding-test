806ebba3edd3e4c082dc160554dd2b9a1bb47934
noaprost <kangjiwon1850@gmail.com>
2023-11-03 15:45:48 +0900

    1541 clear

--- week17/b1541.py
+++ week17/b1541.py
@@ -3,26 +3,32 @@
 import sys
 
 calc = sys.stdin.readline().rstrip()  # 식
-num = ""
-plus = False
-minus = False
 ans = 0
-tmp = 0
-for c in calc:
-    # c가 숫자이면
-    if c != "+" and c != "-":
-        num += c
-    elif c == "-":
-        if not plus and not minus:
-            ans = int(num)
-            num = ""
-        elif not minus:
-            ans -= tmp
-            num = ""
-    elif c == "+":
-        if not plus and not minus:
-            ans = int(num)
-            num = ""
-        elif not plus:
+num = ""
+i = 0
+while i < len(calc) and calc[i] != "-":
+    if calc[i] != "+":
+        num += calc[i]
+    else:
+        ans += int(num)
+        num = ""
+    i += 1
+ans += int(num)
+num = ""
+
+while i < len(calc):
+    tmp = 0
+    num = ""
+    while i < len(calc) and calc[i] != "-":
+        if calc[i] != "+":
+            num += calc[i]
+        else:
             tmp += int(num)
             num = ""
+        i += 1
+    if num != "":
+        tmp += int(num)
+    ans -= tmp
+    i += 1
+
+print(ans)

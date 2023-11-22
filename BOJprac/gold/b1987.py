d8e92c02d99bb5827fe8d3140ee21b958cde87ba
noaprost <kangjiwon1850@gmail.com>
2023-11-22 20:58:56 +0900

    1987 add

--- BOJprac/gold/b1987.py
+++ BOJprac/gold/b1987.py
@@ -7,42 +7,38 @@ h, w = map(int, sys.stdin.readline().split())
 maps = [list(sys.stdin.readline().rstrip()) for _ in range(h)]
 visited = [[False for _ in range(w)] for _ in range(h)]
 
-alph = [maps[0][0]]
+alph = []
 
-que = Queue()
+stack = []
 
-que.put([0, 0])
+stack.append([0, 0])
 
-distance = 1
+distance = 0
 
-while que.qsize() != 0:
-    v = que.get()
+while len(stack) != 0:
+    v = stack.pop()
     x = v[0]
     y = v[1]
-    print(alph)
+    if maps[y][x] not in alph:
+        visited[y][x] = True
+        alph.append(maps[y][x])
+    else:
+        continue
+
     if y > 0 and not visited[y - 1][x] and (maps[y - 1][x] not in alph):
-        que.put([x, y - 1])
-        visited[y - 1][x] = True
-        alph.append(maps[y - 1][x])
-        distance += 1
+        stack.append([x, y - 1])
 
     if y < h - 1 and not visited[y + 1][x] and (maps[y + 1][x] not in alph):
-        que.put([x, y + 1])
-        visited[y + 1][x] = True
-        alph.append(maps[y + 1][x])
-        distance += 1
+        stack.append([x, y + 1])
 
     if x < w - 1 and not visited[y][x + 1] and (maps[y][x + 1] not in alph):
-        que.put([x + 1, y])
-        visited[y][x + 1] = True
-        alph.append(maps[y][x + 1])
-        distance += 1
+        stack.append([x + 1, y])
 
     if x > 0 and not visited[y][x - 1] and (maps[y][x - 1] not in alph):
-        que.put([x - 1, y])
-        visited[y][x - 1] = True
-        alph.append(maps[y][x - 1])
-        distance += 1
+        stack.append([x - 1, y])
 
+    distance += 1
 
+for v in visited:
+    print(v)
 print(distance)

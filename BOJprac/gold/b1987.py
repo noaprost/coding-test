c5ea5700438c4c0e68d4e42d26aba131d97b10bd
noaprost <kangjiwon1850@gmail.com>
2023-11-22 19:40:31 +0900

    1912 clear

--- BOJprac/gold/b1987.py
+++ BOJprac/gold/b1987.py
@@ -1,1 +1,48 @@
 # 알파벳
+import sys
+from queue import Queue
+
+h, w = map(int, sys.stdin.readline().split())
+
+maps = [list(sys.stdin.readline().rstrip()) for _ in range(h)]
+visited = [[False for _ in range(w)] for _ in range(h)]
+
+alph = [maps[0][0]]
+
+que = Queue()
+
+que.put([0, 0])
+
+distance = 1
+
+while que.qsize() != 0:
+    v = que.get()
+    x = v[0]
+    y = v[1]
+    print(alph)
+    if y > 0 and not visited[y - 1][x] and (maps[y - 1][x] not in alph):
+        que.put([x, y - 1])
+        visited[y - 1][x] = True
+        alph.append(maps[y - 1][x])
+        distance += 1
+
+    if y < h - 1 and not visited[y + 1][x] and (maps[y + 1][x] not in alph):
+        que.put([x, y + 1])
+        visited[y + 1][x] = True
+        alph.append(maps[y + 1][x])
+        distance += 1
+
+    if x < w - 1 and not visited[y][x + 1] and (maps[y][x + 1] not in alph):
+        que.put([x + 1, y])
+        visited[y][x + 1] = True
+        alph.append(maps[y][x + 1])
+        distance += 1
+
+    if x > 0 and not visited[y][x - 1] and (maps[y][x - 1] not in alph):
+        que.put([x - 1, y])
+        visited[y][x - 1] = True
+        alph.append(maps[y][x - 1])
+        distance += 1
+
+
+print(distance)
--- BOJprac/silver/b1149.py
+++ BOJprac/silver/b1149.py
@@ -6,10 +6,4 @@ homeNum = int(sys.stdin.readline())
 
 rgbPrice = [list(map(int, sys.stdin.readline().split())) for _ in range(homeNum)]
 
-price = [rgbPrice[0][0], rgbPrice[0][1], rgbPrice[0][2]]
-
-for i in range(1, homeNum):
-    price[0] += min(rgbPrice[i][1], rgbPrice[i][2])
-    price[1] += min(rgbPrice[i][0], rgbPrice[i][2])
-    price[2] += min(rgbPrice[i][0], rgbPrice[i][1])
-    print(price)
+price = [[rgbPrice[0][0], 1], [rgbPrice[0][1], 2], [rgbPrice[0][2], 3]]

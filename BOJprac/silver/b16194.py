0d5e35472dd78cc7eda0aee293c6d81914bcd57b
noaprost <kangjiwon1850@gmail.com>
2023-12-13 16:58:53 +0900

    카드 구매하기2 clear

--- BOJprac/silver/b16194.py
+++ BOJprac/silver/b16194.py
@@ -7,9 +7,9 @@ dp = [0] + list(map(int, sys.stdin.readline().split()))
 
 
 for s in range(2, n + 1):
-    k = 0
-    for i in range(1, s+1):
-        for j in range(i, s+1):
+    k = 10001
+    for i in range(1, s + 1):
+        for j in range(i, s + 1):
             if i + j == s:
                 t = dp[i] + dp[j]
                 k = min(t, k)

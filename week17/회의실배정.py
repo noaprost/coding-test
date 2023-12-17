5c3b8e82ae8993b3fc232032b0dbeaf006ab289b
noaprost <kangjiwon1850@gmail.com>
2023-12-17 17:53:28 +0900

    회의실 배정 드디어 clear

--- week17/회의실배정.py
+++ week17/회의실배정.py
@@ -8,7 +8,13 @@ for _ in range(n):
     a, b = map(int, sys.stdin.readline().split())
     meeting.append([a, b])
 
-meeting.sort(key=lambda x: x[1])
-print(meeting)
+meeting.sort(key=lambda x: (x[1], x[0]))
 
-maxTime = meeting[-1][0]
+count = 1
+endTime = meeting[0][1]
+for i in range(1, n):
+    if meeting[i][0] >= endTime:
+        count += 1
+        endTime = meeting[i][1]
+
+print(count)

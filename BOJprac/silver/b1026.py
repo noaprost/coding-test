6ab80bd06fda207dddd7541d65563b6afceaf2d8
noaprost <kangjiwon1850@gmail.com>
2024-01-16 21:02:33 +0900

    1026, 14494 clear

---
+++ BOJprac/silver/b1026.py
@@ -0,0 +1,17 @@
+# 보물
+import sys
+
+n = int(sys.stdin.readline())
+
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+sum = 0
+
+for aa, bb in zip(a, b):
+    sum += aa * bb
+
+print(sum)

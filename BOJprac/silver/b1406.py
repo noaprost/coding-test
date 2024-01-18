531c6547d39396df25ff53b145ebab39c6381b88
noaprost <kangjiwon1850@gmail.com>
2024-01-18 22:00:28 +0900

    1406 clear

---
+++ BOJprac/silver/b1406.py
@@ -0,0 +1,47 @@
+# 에디터
+import sys
+
+stackLeft = []
+stackRight = []
+
+cm = sys.stdin.readline().rstrip()
+
+for c in cm:
+    stackLeft.append(c)
+
+cmNum = int(sys.stdin.readline())
+
+for _ in range(cmNum):
+    c = sys.stdin.readline().rstrip()
+
+    if c[0] == "P":
+        p, s = c.split()
+        stackLeft.append(s)
+
+    elif c[0] == "L":
+        if len(stackLeft) == 0:
+            continue
+        else:
+            tmp = stackLeft.pop()
+            stackRight.append(tmp)
+
+    elif c[0] == "D":
+        if len(stackRight) == 0:
+            continue
+        else:
+            tmp = stackRight.pop()
+            stackLeft.append(tmp)
+
+    elif c[0] == "B":
+        if len(stackLeft) == 0:
+            continue
+        else:
+            stackLeft.pop()
+
+for sl in stackLeft:
+    print(sl, end="")
+
+stackRight.reverse()
+
+for sr in stackRight:
+    print(sr, end="")

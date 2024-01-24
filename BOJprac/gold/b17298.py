fc346c7e0ab8077edae10aa497b4dd4f4fed1abf
noaprost <kangjiwon1850@gmail.com>
2024-01-24 13:37:49 +0900

    10699 clear

--- BOJprac/gold/b17298.py
+++ BOJprac/gold/b17298.py
@@ -3,24 +3,34 @@ import sys
 
 n = int(sys.stdin.readline())
 
-nums = list(sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
 
 stack = []
 nums.reverse()
 ans = []
+i = 0
 
-while len(nums) != 0:
+while i < n - 1:
     v = nums.pop()
-    if nums[-1] <= v:
-        while nums[-1] < v:
+    if len(nums) != 0 and nums[-1] <= v:
+        while len(nums) != 0 and nums[-1] <= v:
             stack.append(nums.pop())
-        ans.append(nums[-1])
+        if len(nums) == 0:
+            ans.append(-1)
+        else:
+            ans.append(nums[-1])
+            a = nums[-1]
+    elif len(nums) == 0:
+        ans.append(v)
+        break
     else:
         ans.append(nums[-1])
 
     if len(stack) != 0:
-        for s in stack:
-            nums.append(s)
+        while len(stack) != 0:
+            nums.append(stack.pop())
+    i += 1
+
 
 ans.append(-1)
 

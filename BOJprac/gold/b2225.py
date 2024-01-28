24bcbfe75c95eda8d22c3dfbfe106f7fe8c62601
noaprost <kangjiwon1850@gmail.com>
2024-01-28 16:07:01 +0900

    2225, 11053 clear

---
+++ BOJprac/gold/b2225.py
@@ -0,0 +1,12 @@
+# 합분해
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+dp = [[1 for _ in range(n + 1)] for _ in range(k)]
+
+for i in range(1, k):
+    for j in range(1, n + 1):
+        dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000
+
+print(dp[k - 1][n])

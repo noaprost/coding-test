d835ae770f06b71d5b834a5e70666bad5391c497
noaprost <kangjiwon1850@gmail.com>
2024-01-28 15:26:35 +0900

    17298 clear

--- BOJprac/gold/b17298.py
+++ BOJprac/gold/b17298.py
@@ -1,37 +1,20 @@
 # 오큰수
 import sys
+import heapq
 
 n = int(sys.stdin.readline())
 
 nums = list(map(int, sys.stdin.readline().split()))
+ans = [-1] * n
+h = []
 
-stack = []
-nums.reverse()
-ans = []
-i = 0
-
-while i < n - 1:
-    v = nums.pop()
-    if len(nums) != 0 and nums[-1] <= v:
-        while len(nums) != 0 and nums[-1] <= v:
-            stack.append(nums.pop())
-        if len(nums) == 0:
-            ans.append(-1)
+for i in range(n):
+    while len(h) > 0:
+        if nums[i] > h[0][0]:
+            ans[h[0][1]] = nums[i]
+            heapq.heappop(h)
         else:
-            ans.append(nums[-1])
-            a = nums[-1]
-    elif len(nums) == 0:
-        ans.append(v)
-        break
-    else:
-        ans.append(nums[-1])
-
-    if len(stack) != 0:
-        while len(stack) != 0:
-            nums.append(stack.pop())
-    i += 1
-
-
-ans.append(-1)
+            break
+    heapq.heappush(h, (nums[i], i))
 
 print(*ans)

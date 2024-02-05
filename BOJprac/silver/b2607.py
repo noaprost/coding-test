9d98b096179b1523f84ef1caafae2501e15ee97c
noaprost <kangjiwon1850@gmail.com>
2024-02-05 21:52:37 +0900

    2607 clear

---
+++ BOJprac/silver/b2607.py
@@ -0,0 +1,21 @@
+# 비슷한 단어
+import sys
+
+n = int(sys.stdin.readline())
+
+ch = sys.stdin.readline().rstrip()
+similer = 0
+for _ in range(n - 1):
+    com = list(ch)
+    test = sys.stdin.readline().rstrip()
+    count = 0
+
+    for t in test:
+        if t in com:
+            com.remove(t)
+        else:
+            count += 1
+    if count < 2 and len(com) < 2:
+        similer += 1
+
+print(similer)

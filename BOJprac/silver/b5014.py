c7418a33bf8ef15bb09b5954cc2abd816c399036
noaprost <kangjiwon1850@gmail.com>
2024-02-05 21:36:40 +0900

    5014 clear

--- BOJprac/silver/b5014.py
+++ BOJprac/silver/b5014.py
@@ -5,10 +5,51 @@ import sys
 from queue import Queue
 
 totalFloor, start, end, up, down = map(int, sys.stdin.readline().split())
-count = 0
-
-building = [i for i in range(totalFloor + 1)]
-que = Queue()
-
 if start == end:
     exit(print(0))
+
+count = 0
+building = [i for i in range(totalFloor + 1)]
+visited = [True] + [False for _ in range(totalFloor)]
+que = Queue()
+que.put(start)
+visited[start] = True
+while que.qsize() != 0:
+    v = que.get()
+
+    if v == end:
+        exit(print(count))
+
+    if v < end:
+        if up == 0:
+            exit(print("use the stairs"))
+        if v + up <= totalFloor:
+            if not visited[v + up]:
+                que.put(v + up)
+                visited[v + up] = True
+                count += 1
+        elif v - down >= 1:
+            if not visited[v - down]:
+                que.put(v - down)
+                visited[v - down] = True
+                count += 1
+        else:
+            exit(print("use the stairs"))
+
+    if v > end:
+        if down == 0:
+            exit(print("use the stairs"))
+        if v - down >= 1:
+            if not visited[v - down]:
+                que.put(v - down)
+                visited[v - down] = True
+                count += 1
+        elif v + up <= totalFloor:
+            if not visited[v + up]:
+                que.put(v + up)
+                visited[v + up] = True
+                count += 1
+        else:
+            exit(print("use the stairs"))
+
+print("use the stairs")

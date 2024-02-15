7864306db54e652e70101ac9848e46d03a357489
noaprost <kangjiwon1850@gmail.com>
2024-02-15 20:34:35 +0900

    28961 clear

---
+++ BOJprac/bronze/b28691.py
@@ -0,0 +1,15 @@
+# 정보보호학부 동아리 소개
+import sys
+
+c = sys.stdin.readline().rstrip()
+
+if c == "M":
+    print("MatKor")
+elif c == "W":
+    print("WiCys")
+elif c == "C":
+    print("CyKor")
+elif c == "A":
+    print("AlKor")
+elif c == "$":
+    print("$clear")

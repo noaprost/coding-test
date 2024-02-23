bc56881a59ee955f32716fb11e7db295012752db
noaprost <kangjiwon1850@gmail.com>
2024-02-23 18:43:31 +0900

    1010, 11055 clear

---
+++ BOJprac/silver/b1010.py
@@ -0,0 +1,9 @@
+# 다리 놓기
+import sys
+import math
+
+caseNum = int(sys.stdin.readline())
+
+for _ in range(caseNum):
+    r, n = map(int, sys.stdin.readline().split())
+    print(math.comb(n, r))
---
+++ BOJprac/silver/b11055.py
@@ -0,0 +1,20 @@
+# 가장 큰 증가하는 부분 수열
+import sys
+
+n = int(sys.stdin.readline())
+
+num = list(map(int, sys.stdin.readline().split()))
+
+dp = [0 for _ in range(n)]
+dp[0] = num[0]
+
+i = 1
+while i < n:
+    tmp = 0
+    for j in range(i):
+        if tmp < dp[j] and num[j] < num[i]:
+            tmp = dp[j]
+    dp[i] = tmp + num[i]
+    i += 1
+
+print(max(dp))

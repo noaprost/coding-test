0424b6ceea3e775aef201c39dad1d8a4c6f9d3d2
noaprost <kangjiwon1850@gmail.com>
2024-02-26 00:36:39 +0900

    9653 clear

--- BOJprac/gold/내리막길.py
+++ BOJprac/gold/내리막길.py
@@ -8,5 +8,16 @@ maps = []
 for _ in range(m):
     maps.append(list(map(int, sys.stdin.readline().split())))
 
-# idea
-# 
+# 주변에 있는 애들을 어떻게 넣지
+
+
+def dfs(graph, node, visited):
+
+    for i in graph:
+        visited[i] = True
+        dfs(graph, i, visited)
+        visited[i] = False
+
+
+# dfs
+# 방문하고나서 방문처리를 풀어줘야 모든 경로를 찾을 수 있음

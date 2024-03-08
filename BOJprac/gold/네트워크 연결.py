0fae2eb893a8a5b66db196699e9e7a5566409f7a
noaprost <kangjiwon1850@gmail.com>
2024-03-08 12:20:08 +0900

    네트워크 연결 clear

---
+++ BOJprac/gold/네트워크 연결.py
@@ -0,0 +1,29 @@
+# 1922
+import sys
+import heapq
+
+vnum = int(sys.stdin.readline())
+enum = int(sys.stdin.readline())
+
+visited = [False] * (vnum + 1)
+edgeList = [[] for _ in range(vnum + 1)]
+heap = [[0, 1]]
+for _ in range(enum):
+    s, e, w = map(int, sys.stdin.readline().split())
+    edgeList[s].append([w, e])
+    edgeList[e].append([w, s])
+
+totalWeight = 0
+count = 0
+while heap:
+    if count == vnum:
+        break
+    w, s = heapq.heappop(heap)
+    if not visited[s]:
+        visited[s] = True
+        totalWeight += w
+        count += 1
+        for edge in edgeList[s]:
+            heapq.heappush(heap, edge)
+
+print(totalWeight)

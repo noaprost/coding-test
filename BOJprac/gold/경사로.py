9eab1a5a8ecc1e60ba6427cb1bf9cdc3dcac402e
noaprost <kangjiwon1850@gmail.com>
2024-04-11 16:06:17 +0900

    경사로 clear

--- BOJprac/gold/경사로.py
+++ BOJprac/gold/경사로.py
@@ -8,24 +8,122 @@ maps = []
 for _ in range(n):
     maps.append(list(map(int, sys.stdin.readline().split())))
 
-# 가로길 검사
-for i in range(n - 1):
-    Lcount = 1
-    for j in range(n - 1):
-        if maps[i][j] == maps[i][j + 1]:
-            Lcount += 1
-        # 현재 높이가 다음 길의 높이보다 높다면 1.높이 검사 2.뒤로 L만큼의 평지가 유지되는가 -> for문으로 검사하지 않고 flag를 세워야되나
-        if maps[i][j] > maps[i][j + 1]:
-            if maps[i][j] - maps[i][j + 1] != 1:
-                break
+ans = 0
 
-            for k in range(1, L + 1):
-                if maps[i][j + 1] != maps[i][j + k]:
+# 가로길 검사
+for i in range(n):
+    j = 1
+    curHeight = maps[i][0]
+    flag = 1
+    visited = [False] * n
+    while j < n:
+        if abs(curHeight - maps[i][j]) > 1:
+            flag = 0
+            break
+        else:
+            if curHeight == maps[i][j]:
+                j += 1
+
+            elif curHeight < maps[i][j]:
+                tmpHeight = maps[i][j - 1]
+
+                for k in range(0, L):
+                    if j - k - 1 >= 0:
+                        if not visited[j - k - 1]:
+                            if tmpHeight != maps[i][j - k - 1]:
+                                flag = 0
+                            else:
+                                visited[j - k - 1] = True
+                        else:
+                            flag = 0
+                    else:
+                        flag = 0
+
+                if not flag:
                     break
-            j += 2
-            Lcount = 0
-        # 현재 높이가 다음 길의 높이보다 낮다면 1.높이 검사 2.지나온 길의 평지가 L만큼 유지되었는가 -> 이건 현재 평지가 몇만큼 이어졌는지 카운트를 해줌
+                else:
+                    curHeight = maps[i][j]
+                    j += 1
+
+            elif curHeight > maps[i][j]:
+                tmpHeight = maps[i][j]
+
+                for k in range(0, L):
+                    if j + k < n:
+                        if not visited[j + k]:
+                            if tmpHeight != maps[i][j + k]:
+                                flag = 0
+                            else:
+                                visited[j + k] = True
+                        else:
+                            flag = 0
+                    else:
+                        flag = 0
+
+                if not flag:
+                    break
+                else:
+                    curHeight = maps[i][j]
+                    j += L
+    if flag:
+        ans += 1
 
 # 세로길 검사
-# for i in range(n):
-#     for j in range(n):
+for i in range(n):
+    j = 1
+    curHeight = maps[0][i]
+    flag = 1
+    visited = [False] * n
+    while j < n:
+        if abs(curHeight - maps[j][i]) > 1:
+            flag = 0
+            break
+        else:
+            if curHeight == maps[j][i]:
+                j += 1
+
+            elif curHeight < maps[j][i]:
+                tmpHeight = maps[j - 1][i]
+
+                for k in range(0, L):
+                    if j - k - 1 >= 0:
+                        if not visited[j - k - 1]:
+                            if tmpHeight != maps[j - k - 1][i]:
+                                flag = 0
+                            else:
+                                visited[j - k - 1] = True
+                        else:
+                            flag = 0
+                    else:
+                        flag = 0
+
+                if not flag:
+                    break
+                else:
+                    curHeight = maps[j][i]
+                    j += 1
+
+            elif curHeight > maps[j][i]:
+                tmpHeight = maps[j][i]
+
+                for k in range(0, L):
+                    if j + k < n:
+                        if not visited[j + k]:
+                            if tmpHeight != maps[j + k][i]:
+                                flag = 0
+                            else:
+                                visited[j + k] = True
+                        else:
+                            flag = 0
+                    else:
+                        flag = 0
+
+                if not flag:
+                    break
+                else:
+                    curHeight = maps[j][i]
+                    j += L
+    if flag:
+        ans += 1
+
+print(ans)

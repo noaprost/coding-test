45cc52a9f816d6061900ad20891349581e364f27
noaprost <kangjiwon1850@gmail.com>
2024-04-28 17:43:08 +0900

    치킨 배달

--- BOJprac/gold/치킨 배달.py
+++ BOJprac/gold/치킨 배달.py
@@ -1,16 +1,28 @@
 # 15686
 import sys
-from collections import deque
+from itertools import combinations
 
 n, m = map(int, sys.stdin.readline().split())
 
 maps = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-peopleLoc = deque()
-chickenLoc = deque()
+peopleLoc = []
+chickenLoc = []
+ans = 1e8
 
 for i in range(n):
     for j in range(n):
         if maps[i][j] == 1:
             peopleLoc.append([i, j])
         elif maps[i][j] == 2:
-            chickenLoc([i, j])
+            chickenLoc.append([i, j])
+
+for chi in combinations(chickenLoc, m):
+    totalLen = 0
+    for p in peopleLoc:
+        chickenLen = 1e8
+        for j in range(m):
+            chickenLen = min(chickenLen, abs(chi[j][0] - p[0]) + abs(chi[j][1] - p[1]))
+        totalLen += chickenLen
+    ans = min(ans, totalLen)
+
+print(ans)

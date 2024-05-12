e1421debd65e095249ff7eff1db77ce612577ac0
noaprost <kangjiwon1850@gmail.com>
2024-05-12 23:26:43 +0900

    임계경로 clear

--- BOJprac/platinum/임계경로.py
+++ BOJprac/platinum/임계경로.py
@@ -1,45 +1,59 @@
 # 1948
-# import sys
-# from collections import deque
+import sys
+from collections import deque
 
-# n = int(sys.stdin.readline())
-# m = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
 
-# graph = [[] for _ in range(n + 1)]
-# degree = [1e8] + [0 for _ in range(n)]
-# d = [0]
-# dq = deque()
-# dp = [0 for _ in range(n + 1)]
-# dist = 0
+graph = [[] for _ in range(n + 1)]
+degree = [1e8] + [0 for _ in range(n)]
+dq = deque()
+dp = [0 for _ in range(n + 1)]
+dist = [[] for _ in range(n + 1)]
 
-# for _ in range(m):
-#     s, e, w = map(int, sys.stdin.readline().split())
-#     graph[s].append(e)
-#     d.append(w)
+for i in range(m):
+    s, e, w = map(int, sys.stdin.readline().split())
+    graph[s].append((e, w, i))
+    degree[e] += 1
 
-# s, e = map(int, sys.stdin.readline().split())
-# tmp = deque()
-# tmp.append(s)
+s, e = map(int, sys.stdin.readline().split())
 
-# while tmp:
-#     v = tmp.popleft()
+dq.append(s)
 
-#     for g in graph[v]:
-#         degree[g] += 1
-#         tmp.append(g)
+while dq:
+    v = dq.popleft()
 
-# dq.append(s)
-# dp[s] = d[s]
+    if v == e:
+        break
 
-# while dq:
-#     v = dq.popleft()
+    for g in graph[v]:
+        degree[g[0]] -= 1
 
-#     for g in graph[v]:
-#         degree[g] -= 1
-#         dp[g] = max(dp[v] + d[g], dp[g])
-#         if degree[g] == 0:
-#             dq.append(g)
-#             dist += 1
+        if dp[v] + g[1] > dp[g[0]]:
+            dist[g[0]] = [(g[2], v)]
+            dp[g[0]] = dp[v] + g[1]
 
-# print(dp)
-# print(dist)
+        elif dp[v] + g[1] == dp[g[0]]:
+            dist[g[0]].append((g[2], v))
+
+        if degree[g[0]] == 0:
+            dq.append(g[0])
+
+ans = set()
+que = deque()
+visited = [False for _ in range(m)]
+for s in dist[e]:
+    que.append(s)
+    visited[s[0]] = True
+
+while que:
+    bri, pre = que.popleft()
+    ans.add(bri)
+
+    for d in dist[pre]:
+        if not visited[d[0]]:
+            que.append(d)
+            visited[d[0]] = True
+
+print(dp[e])
+print(len(ans))

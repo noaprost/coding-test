97773e3163bb2a119df83f0719abea92710c2f3d
noaprost <kangjiwon1850@gmail.com>
2024-05-15 23:50:43 +0900

    사이클 게임 clear

---
+++ BOJprac/gold/사이클 게임.py
@@ -0,0 +1,33 @@
+# 20040
+import sys
+
+sys.setrecursionlimit(10**8)
+
+n, m = map(int, sys.stdin.readline().split())
+
+parent = [i for i in range(n)]
+
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x != y:
+        parent[x] = y
+
+
+ans = 0
+for i in range(1, m + 1):
+    s, e = map(int, sys.stdin.readline().split())
+
+    if find(s) == find(e):
+        exit(print(i))
+        break
+    union(s, e)
+print(0)

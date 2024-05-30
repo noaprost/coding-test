d406f5c6006c4a30e5572fa05182d1f76bcf9644
noaprost <kangjiwon1850@gmail.com>
2024-05-30 23:13:32 +0900

    30679 clear

---
+++ BOJprac/bronze/b30679.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+if(620 <= n):
+    print("Red")
+elif(590 <= n):
+    print("Orange")
+elif(570 <= n):
+    print("Yellow")
+elif(495 <= n):
+    print("Green")
+elif(450 <= n):
+    print("Blue")
+elif(425 <= n):
+    print("Indigo")
+elif(380 <= n):
+    print("Violet")
+
+    

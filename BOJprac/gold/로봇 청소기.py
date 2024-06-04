41c1062f4f2d2f6b8466bb1730f1291f0a2fbb97
noaprost <kangjiwon1850@gmail.com>
2024-06-05 01:05:55 +0900

    로봇청소기 clear

---
+++ BOJprac/gold/로봇 청소기.py
@@ -0,0 +1,40 @@
+# 14503
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+sx, sy, d = map(int, input().split())
+maps = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+visited[sx][sy] = True
+c = 1
+
+que = deque()
+que.append((sx, sy))
+while que:
+    x, y = que.popleft()
+    # 4방향에 청소되지 않은 빈칸이 있는지 확인
+    for _ in range(4):
+        # 반시계 방향 회전
+        d = (d + 3) % 4
+
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 0 and not visited[nx][ny]:
+            visited[nx][ny] = True
+            que.append((nx, ny))
+            c += 1
+            break
+
+    if not que:
+        if maps[x - dx[d]][y - dy[d]] == 1:
+            exit(print(c))
+        else:
+            que.append((x - dx[d], y - dy[d]))

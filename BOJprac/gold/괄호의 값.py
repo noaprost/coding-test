79a4f4c997d6d60e18a2a5aeea8af5e601dfeead
noaprost <kangjiwon1850@gmail.com>
2024-06-13 15:44:07 +0900

    괄호의 값 clear

--- BOJprac/gold/괄호의 값.py
+++ BOJprac/gold/괄호의 값.py
@@ -1,37 +1,41 @@
 # 2504
 import sys
 
-bracket = sys.stdin.readline().rstrip()
-
-s = input()
+bracket = list(sys.stdin.readline().rstrip())
 stack = []
 ans = 0
+tmp = 1
 
-for ss in s:
-    if ss == "(":
-        stack.append(ss)
-    elif ss == "[":
-        stack.append(ss)
-    elif ss == ")":
-        if len(stack) == 0:
-            exit(print(0))
-        else:
-            tmp = stack.pop()
-            if tmp == "(":
-                continue
-            else:
-                exit(print(0))
-    elif ss == "]":
-        if len(stack) == 0:
-            exit(print(0))
-        else:
-            tmp = stack.pop()
-            if tmp == "[":
-                continue
-            else:
-                exit(print(0))
+for i in range(len(bracket)):
+    if bracket[i] == "(":
+        stack.append(bracket[i])
+        tmp *= 2
 
-if len(stack) != 0:
-    exit(print(0))
+    elif bracket[i] == "[":
+        stack.append(bracket[i])
+        tmp *= 3
+
+    elif bracket[i] == ")":
+        if not stack or stack[-1] == "[":
+            exit(print(0))
+
+        if bracket[i - 1] == "(":
+            ans += tmp
+
+        stack.pop()
+        tmp //= 2
+
+    elif bracket[i] == "]":
+        if not stack or stack[-1] == "(":
+            exit(print(0))
+
+        if bracket[i - 1] == "[":
+            ans += tmp
+
+        stack.pop()
+        tmp //= 3
+
+if stack:
+    print(0)
 else:
-    exit(print(ans))
+    print(ans)

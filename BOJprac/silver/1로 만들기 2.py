98a5dabc7edd4e4d14c6873432be6f4fd226ca41
noaprost <kangjiwon1850@gmail.com>
2024-06-29 17:59:03 +0900

    운동 clear

--- BOJprac/silver/1로 만들기 2.py
+++ BOJprac/silver/1로 만들기 2.py
@@ -1,36 +1,36 @@
 import sys
-from queue import Queue
+from collections import deque
 
 n = int(sys.stdin.readline())
 
 # 그래프 탐색 시작 (BFS 이용)
-que = Queue()
+que = deque()
 visited = [False] * (n + 1)
 distCheck = [0] * (n + 1)
 
-que.put([n, 0])
+que.append((n, 0))
 
 dist = 0
 
-while que.qsize() != 0:
-    v = que.get()
+while que:
+    v = que.popleft()
 
     if v[0] == 1:
         dist = v[1]
         break
 
     if v[0] % 3 == 0 and not visited[v[0] // 3]:
-        que.put([v[0] // 3, v[1] + 1])
+        que.append((v[0] // 3, v[1] + 1))
         visited[v[0] // 3] = True
         distCheck[v[0] // 3] = v[0]
 
     if v[0] % 2 == 0 and not visited[v[0] // 2]:
-        que.put([v[0] // 2, v[1] + 1])
+        que.append((v[0] // 2, v[1] + 1))
         visited[v[0] // 2] = True
         distCheck[v[0] // 2] = v[0]
 
     if not visited[v[0] - 1]:
-        que.put([v[0] - 1, v[1] + 1])
+        que.append((v[0] - 1, v[1] + 1))
         visited[v[0] - 1] = True
         distCheck[v[0] - 1] = v[0]
 

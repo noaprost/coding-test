6005acb72e5568ff914c5d0fa3ca054bb5bb597b
noaprost <kangjiwon1850@gmail.com>
2024-06-29 20:37:05 +0900

    숫자고르기 clear

--- BOJprac/gold/숫자고르기.py
+++ BOJprac/gold/숫자고르기.py
@@ -1,7 +1,25 @@
 # 2668
 import sys
-from itertools import permutations
+
+
+def dfs(v, s):
+    visited[v] = True
+    w = base[v]
+    if not visited[w]:
+        dfs(w, s)
+    elif visited[w] and w == s:
+        ans.append(w)
+
 
 n = int(sys.stdin.readline())
-base = [i for i in range(1, n + 1)]
-nums = [0] + [int(sys.stdin.readline()) for _ in range(n)]
+base = [0] + [int(sys.stdin.readline()) for _ in range(n)]
+ans = []
+
+for i in range(1, n + 1):
+    visited = [True] + [False for _ in range(n)]
+    dfs(i, i)
+
+ans.sort()
+print(len(ans))
+for a in ans:
+    print(a)

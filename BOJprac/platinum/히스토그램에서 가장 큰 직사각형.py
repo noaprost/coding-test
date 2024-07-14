9efcfe22fcaccbac3f737fa9a6a378128ffeac84
noaprost <kangjiwon1850@gmail.com>
2024-07-14 23:32:03 +0900

    히스토그램에서 가장 큰 직사각형 clear

---
+++ BOJprac/platinum/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,37 @@
+# 6549
+import sys
+
+while True:
+    n, *q = map(int, sys.stdin.readline().split())
+
+    if n == 0:
+        exit()
+
+    stack = []
+    max_res = 0
+
+    for i, h in enumerate(q):
+
+        if stack and stack[-1][1] > h:
+            while stack:
+                _, s_h = stack.pop()
+                w = 1
+                if stack:
+                    w = stack[-1][0] + 1
+                res = (i + 1 - w) * s_h
+                max_res = max(res, max_res)
+                if not stack or stack[-1][1] <= h:
+                    break
+
+        if not stack or stack[-1][1] <= h:
+            stack.append((i + 1, h))
+
+    while stack:
+        _, s_h = stack.pop()
+        w = 1
+        if stack:
+            w = stack[-1][0] + 1
+        res = (n + 1 - w) * s_h
+        max_res = max(res, max_res)
+
+    print(max_res)

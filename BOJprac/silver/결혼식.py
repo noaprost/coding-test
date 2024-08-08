a1bb61ae9e689bdf14f0212ec2dc0e9296e20c50
noaprost <kangjiwon1850@gmail.com>
2024-08-08 16:44:56 +0900

    결혼식 - 코드 수정

--- BOJprac/silver/결혼식.py
+++ BOJprac/silver/결혼식.py
@@ -1,14 +1,11 @@
 import sys
-from queue import Queue
+from collections import deque
 
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-
 visited = [False for _ in range(n + 1)]
-
 graph = [[] for _ in range(n + 1)]
-
-que = Queue()
+que = deque()
 
 for _ in range(m):
     s, e = map(int, sys.stdin.readline().split())
@@ -22,12 +19,12 @@ if len(graph[1]) == 0:
 else:
     visited[1] = True
     for g in graph[1]:
-        que.put(g)
+        que.append(g)
         visited[g] = True
         count += 1
 
-while que.qsize() != 0:
-    v = que.get()
+while que:
+    v = que.popleft()
     visited[v] = True
     for g in graph[v]:
         if not visited[g]:

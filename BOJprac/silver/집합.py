4d662ce8aeb4605ae84ad88e53fdc2f8b8c3f439
noaprost <kangjiwon1850@gmail.com>
2024-08-22 16:23:08 +0900

    집합 clear

--- BOJprac/silver/집합.py
+++ BOJprac/silver/집합.py
@@ -1,47 +1,36 @@
-# add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
-# remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
-# check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
-# toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
-# all: S를 {1, 2, ..., 20} 으로 바꾼다.
-# empty: S를 공집합으로 바꾼다.
+# 11723
+import sys
 
+commandNum = int(sys.stdin.readline())
+s = set()
 
-# 틀린 문제
+for _ in range(commandNum):
+    command = sys.stdin.readline().strip().split()
 
+    if command[0] == "add":
+        c, v = command
+        s.add(int(v))
 
-# import sys
+    if command[0] == "remove":
+        c, v = command
+        s.discard(int(v))
 
-# commandNum = int(sys.stdin.readline())
+    if command[0] == "check":
+        c, v = command
+        if int(v) in s:
+            print(1)
+        else:
+            print(0)
 
-# s = set()
+    if command[0] == "toggle":
+        c, v = command
+        if int(v) in s:
+            s.remove(int(v))
+        else:
+            s.add(int(v))
 
-# for _ in range(commandNum):
-#     command = sys.stdin.readline().strip()
+    if command[0] == "all":
+        s = set([i for i in range(1, 21)])
 
-#     if command == "add":
-#         c, v = command.split()
-#         if int(v) not in s:
-#             s.add(int(v))
-
-#     if command == "remove":
-#         c, v = command.split()
-#         if int(v) in s:
-#             s.remove(int(v))
-
-#     if command == "check":
-#         c, v = command.split()
-#         if int(v) in s:
-#             print(1)
-#         else:
-#             print(0)
-#     if command == "toggle":
-#         if int(v) in s:
-#             s.remove(int(v))
-#         else:
-#             s.add(int(v))
-
-#     if command == "all":
-#         s = set([i for i in range(1, 21)])
-
-#     if command == "empty":
-#         s = set([])
+    if command[0] == "empty":
+        s = set([])

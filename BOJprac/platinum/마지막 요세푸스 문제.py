3d57d3927924d873e630992a85b5bd47359dcaa6
noaprost <kangjiwon1850@gmail.com>
2024-08-29 18:27:59 +0900

    마지막 요세푸스 문제 clear

--- BOJprac/platinum/마지막 요세푸스 문제.py
+++ BOJprac/platinum/마지막 요세푸스 문제.py
@@ -1,17 +1,38 @@
 # 1179
-# import sys
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+ans = 0
+nn = 1
+if k == 1:
+    print(n)
+else:
+    while True:
+        x = (nn - ans - 1) // (k - 1) + 1
+        if nn + x > n:
+            ans += (n - nn) * k
+            break
+        ans = (ans + k * x) % (nn + x)
+        nn += x
+    print(ans + 1)
+
+# sys.setrecursionlimit(10**9)
+
+
+# def Josephus(n, k):
+#     if n == 1:
+#         return 0
+#     if k == 1:
+#         return n - 1
+#     if k <= n:
+#         n2 = n - n / k
+#         tmp = Josephus(n2, k) - n % k
+#         if tmp < 0:
+#             tmp += n2
+#         return (k * (tmp % n2)) / (k - 1)
+#     else:
+#         return (Josephus(n - 1, k) + k) % n
+
 
 # n, k = map(int, sys.stdin.readline().split())
-
-# que = [i for i in range(1, n + 1)]
-
-# front = 0
-
-# while len(que) != 1:
-#     front += k - 1
-#     front = front % len(que)
-#     que.remove(que[front])
-
-# print(que[0])
-
-# 메모리초과
+# print(Josephus(n, k) + 1)
